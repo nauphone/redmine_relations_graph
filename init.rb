60f0873a232b65c080e7400aff4fef7649c0faa1
Alex Chrome <dify.chrome@gmail.com>
2012-03-11 11:06:21 +0700

    Added ruby-graphviz as plugin

--- init.rb
+++ init.rb
@@ -3,6 +3,13 @@ require 'redmine'
 
 require 'dispatcher'
 
+Dir[File.join(directory,'vendor','plugins','*')].each do |dir|
+  path = File.join(dir, 'lib')
+  $LOAD_PATH << path
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
+end
+
 require_dependency 'issues_controller_patch'
 require_dependency 'issue_relation_chart'
 require_dependency 'view_hook'
